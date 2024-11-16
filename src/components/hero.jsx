60299cbd268f17cc2ac07ef396d172f2195aae47
pranavk174 <144471197+pranavk174@users.noreply.github.com>
2024-11-16 20:19:57 +0530

    title change

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -32,7 +32,7 @@ export default function Hero() {
               animate="visible"
               className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
             >
-              Full Stack Developer
+              MERN Stack Developer
             </motion.span>
             <motion.p
               variants={container (0.5)} initial="hidden" animate="visible"
